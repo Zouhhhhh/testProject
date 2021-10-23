9c61137c01c328ab769cd7d104b1c3df8c33e0e5
zouhao <1179036329@qq.com>
2021-10-23 12:39:42 +0800

    提交代理模式Proxy学习内容

--- project-be/spring-study/spring-08-proxy/src/main/java/com/kuang/demo01/Client.java
+++ project-be/spring-study/spring-08-proxy/src/main/java/com/kuang/demo01/Client.java
@@ -1,7 +1,7 @@
 package com.kuang.demo01;
 
 public class Client {
-    public static void main(String[] args) {
+    public void test1() {
         //房东要出租房子
         Host host = new Host();
         //代理，中介帮房东出租房子，但是代理角色会有一些附属操作（收中介费）
---
+++ project-be/spring-study/spring-08-proxy/src/main/java/com/kuang/onlytest/Teacher.java
@@ -0,0 +1,7 @@
+package com.kuang.onlytest;
+
+public class Teacher extends Person {
+    public Teacher() {
+        this.name = "老师";
+    }
+}

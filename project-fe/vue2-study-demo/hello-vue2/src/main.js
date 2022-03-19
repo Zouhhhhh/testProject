d533cc1b9f6017f7be5a8b93cdc522d800538d18
zouhao <1179036329@qq.com>
2022-03-19 23:23:20 +0800

    学习资料

--- project-fe/vue2-study-demo/hello-vue2/src/main.js
+++ project-fe/vue2-study-demo/hello-vue2/src/main.js
@@ -6,6 +6,22 @@ Vue.config.productionTip = false
 console.log("当前环境", process.env.NODE_ENV);
 console.log("当前后端地址", process.env.VUE_APP_URL);
 
+
+//创建Vue实例
+const x = new Vue({
+  //el: '#app',   //el用于指定当前Vue实例为哪个容器服务，值通常为css选择器字符串
+  // render: h => h(App)
+});
+
+
+//create project时自动生成的
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+//可以写成
+// new Vue({
+//   el: '#app',
+//   render: h => h(App),
+// })

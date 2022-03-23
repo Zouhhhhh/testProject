0ec049bd2f2d508c467e99bbef26a5859e671964
zouhao <zouhao@zsoft.com.cn>
2022-03-23 21:49:45 +0800

    servlet文件上传，输入流输出流

--- project-be/servlet-file/README.md
+++ project-be/servlet-file/README.md
@@ -6,3 +6,46 @@ Servlet需要注意的：
     1. UpLoadServlet接收
     2. 不能以parameter形式去取，会null
     3. 需要以流的方式去取（req.getInputStream()）
+
+读取文本：
+    输入流
+    InputStream inputStream = req.getInputStream();
+    Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+    BufferedReader bufferedReader = new BufferedReader(reader);
+    String str = "";
+    while((str = bufferedReader.readLine()) != null) {
+        System.out.println(str);
+    }
+
+读取文本并保存
+    1. 通过输入流获取客户端传来的数据流
+        代码同上(但是要注释掉读取输入流，流只能被读取一次！！！)
+    2. 通过输出流将数据流输出到本地硬盘
+        需要先在out/artifacts/.../创建一个file，就是工程的web文件夹下
+        获取文件的绝对路径（可以通过ServletContext去找，其实找的是打包后的out/里面）
+        
+        String filePath = req.getServletContext().getRealPath("file/copy.txt");
+        OutputStream outputStream = new FileOutputStream(filePath);
+        Writer writer = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
+        BufferedWriter bufferedWriter = new BufferedWriter(writer);
+        while((str = bufferedReader.readLine()) != null) {
+            bufferedWriter.write(str);
+        }
+
+使用框架（fileupload）完成文件上传
+    1. 导包，WEB-INF下创建lib
+    2. 
+    
+
+
+//TODO:（还是回去看书吧，网上的太杂了）
+java的I/O流学习
+    1. 字节流byte
+        - InputStream
+            - read()读取下一个字节
+            - read(byte[])读取全部字节放到byte数组
+            - read(byte[], off, len)读取从off开始后的len个字节
+        - outputStream
+    2. 字符流
+        - Reader
+        - Writer
--- project-be/servlet-file/src/com/zouhhhhh/servlet/UpLoadServlet.java
+++ project-be/servlet-file/src/com/zouhhhhh/servlet/UpLoadServlet.java
@@ -1,27 +1,64 @@
 package com.zouhhhhh.servlet;
 
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 @WebServlet("/upload")
 public class UpLoadServlet extends HttpServlet {
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        String img = req.getParameter("img");
 //        System.out.println(img);      //null
+//    }
 
-        InputStream inputStream = req.getInputStream();
-        int temp = 0;
-        while((temp = inputStream.read()) != -1) {
-            System.out.println(temp);
-        }
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        InputStream inputStream = req.getInputStream();
+//        Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+//        BufferedReader bufferedReader = new BufferedReader(reader);
+//        String str = "";
+//        while((str = bufferedReader.readLine()) != null) {
+//            System.out.println(str);
+//        }
+//    }
 
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        //通过输入流获取客户端传来的数据流
+//        InputStream inputStream = req.getInputStream();
+//        Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+//        BufferedReader bufferedReader = new BufferedReader(reader);
+//        String str = "";
+//
+//        //通过输出流将数据流输出到本地硬盘
+//        String filePath = req.getServletContext().getRealPath("file/copy.txt");
+//        OutputStream outputStream = new FileOutputStream(filePath);
+//        Writer writer = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
+//        BufferedWriter bufferedWriter = new BufferedWriter(writer);
+//        while((str = bufferedReader.readLine()) != null) {
+//            bufferedWriter.write(str);
+//        }
+//
+//        //关闭流
+//        bufferedWriter.close();
+//        writer.close();
+//        bufferedReader.close();
+//        reader.close();
+//        inputStream.close();
+//    }
 
-
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String img = req.getParameter("img");
+        System.out.println(img);      //null
     }
+
 }

1f4df52196db95cc50296e2766f59cfcab21c040
zouhao <1179036329@qq.com>
2022-03-28 23:14:32 +0800

    数据库连接池c3p0

---
+++ project-be/javaweb/5-jdbc-c3p0/src/com/zouhhhhh/test/DataSourceTest.java
@@ -0,0 +1,27 @@
+package com.zouhhhhh.test;
+
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSourceTest {
+
+    public static void main(String[] args) {
+        try {
+            //创建C3P0
+            ComboPooledDataSource dataSource = new ComboPooledDataSource();
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/test?useSSL=FALSE&serverTimezone=UTC&allowPublicKeyRetrieval=true");
+            dataSource.setUser("root");
+            dataSource.setPassword("root");
+            Connection connection = dataSource.getConnection();
+            connection.close();     //这里的close不是关闭，而是放回dataSource连接池
+        } catch (PropertyVetoException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}

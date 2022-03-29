b6800f170f9289a2784465576be56a64cdafa4a9
zouhao <1179036329@qq.com>
2022-03-29 23:23:28 +0800

    用配置文件设置c3p0数据库连接池

--- project-be/javaweb/5-jdbc-c3p0/src/com/zouhhhhh/test/DataSourceTest.java
+++ project-be/javaweb/5-jdbc-c3p0/src/com/zouhhhhh/test/DataSourceTest.java
@@ -9,17 +9,33 @@ import java.sql.SQLException;
 
 public class DataSourceTest {
 
+//    public static void main(String[] args) {
+//        try {
+//            //创建C3P0
+//            ComboPooledDataSource dataSource = new ComboPooledDataSource();
+//            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+//            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/test?useSSL=FALSE&serverTimezone=UTC&allowPublicKeyRetrieval=true");
+//            dataSource.setUser("root");
+//            dataSource.setPassword("root");
+//            Connection connection = dataSource.getConnection();
+//            System.out.println(connection);
+//            connection.close();     //这里的close不是关闭，而是放回dataSource连接池
+//        } catch (PropertyVetoException | SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
+    //根据配置文件加载
     public static void main(String[] args) {
         try {
             //创建C3P0
-            ComboPooledDataSource dataSource = new ComboPooledDataSource();
-            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
-            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/test?useSSL=FALSE&serverTimezone=UTC&allowPublicKeyRetrieval=true");
-            dataSource.setUser("root");
-            dataSource.setPassword("root");
+            ComboPooledDataSource dataSource = new ComboPooledDataSource("testc3p0");
+
             Connection connection = dataSource.getConnection();
+            System.out.println(connection);
             connection.close();     //这里的close不是关闭，而是放回dataSource连接池
-        } catch (PropertyVetoException | SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }

99ae5275ff0b8bc5873c6ed9af10ed96f0a0299d
zouhao <1179036329@qq.com>
2022-05-28 11:04:06 +0800

    mybatis-级联-延迟加载（补充slf4j的jar包，打印日志）

--- project-be/3-mybatis/9-mapper-plus/src/cascade/test/Test.java
+++ project-be/3-mybatis/9-mapper-plus/src/cascade/test/Test.java
@@ -4,17 +4,23 @@ import cascade.mapper.*;
 import cascade.pojo.*;
 import cascade.utils.SqlSessionFactoryUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 
 public class Test {
 
     public static void main(String[] args) {
+        try {
+            Logger logger = Logger.getLogger(Test.class);
+            SqlSession sqlSession = SqlSessionFactoryUtils.openSqlSession();
+            EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+            Employee maleEmployee = mapper.getEmployeeById(1L);
+            logger.info(maleEmployee.getBirthday());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        SqlSession sqlSession = SqlSessionFactoryUtils.openSqlSession();
-        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
-        Employee femaleEmployee = mapper.getEmployeeById(2L);
-        System.out.println(femaleEmployee.getSex());
 
     }
 

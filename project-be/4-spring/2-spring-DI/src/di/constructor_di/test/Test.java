e3afa13cf3a0799bdf45a5348a806c6a993fb41b
zouhao <1179036329@qq.com>
2022-06-12 12:54:41 +0800

    spring-2-依赖注入之构造器注入

---
+++ project-be/4-spring/2-spring-DI/src/di/constructor_di/test/Test.java
@@ -0,0 +1,14 @@
+package di.constructor_di.test;
+
+import di.constructor_di.pojo.Role;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("di/constructor_di/applicationContext.xml");
+        Role role = ctx.getBean(Role.class, "role");
+        System.out.println(role.getRoleName());
+        System.out.println(role.getNote());
+    }
+}

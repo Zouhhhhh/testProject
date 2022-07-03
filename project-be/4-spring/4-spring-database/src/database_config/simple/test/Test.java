02d44c6397cb1977cce8a679ad0a0313974f5664
zouhao <1179036329@qq.com>
2022-07-03 12:56:59 +0800

    spring-4-配置数据源的三种方式（本地，第三方，服务器）

---
+++ project-be/4-spring/4-spring-database/src/database_config/simple/test/Test.java
@@ -0,0 +1,32 @@
+package database_config.simple.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("database_config/simple/applicationContext.xml");
+        DataSource dataSource = ctx.getBean(DataSource.class, "dataSource");
+        ResultSet resultSet = null;
+        String sql = "SELECT * FROM user";
+        try(Connection connection = dataSource.getConnection();PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String id = resultSet.getString(1);
+                String name = resultSet.getString(2);
+                System.out.println(id + " - " + name);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
